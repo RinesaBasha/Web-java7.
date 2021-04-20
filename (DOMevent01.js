faebcfabe02389ed71cdb7ca8dd3bac97df25f53
Rinesa Basha <rinesa.basha@gmail.com>
2021-04-20 09:30:29 +0200

    init

---
+++ (DOMevent01.js
@@ -0,0 +1,8 @@
+var shfaq=()=>{
+    var lenda= document.getElementById('texti').value;
+    var mesazhi = "<h3> Sot kemi ligjerata "+ lenda +" !</h3>";
+    // document.getElementById('kontenti')
+    // .textContent=mesazhi;
+    document.querySelector('#kontenti')
+    .innerHTML=mesazhi;
+    }
---
+++ testPHPG1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Programi i pare ne PHP</title>
+        <style>
+            div{
+                max-width:300px;
+                min-height:300px;
+                background-color:#00ff11;
+                margin: o auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <h2>Me poshte eshte paraqitur kodomi ne PHP</h2
+            <?php
+                echo "<h3>Mire se vini ne programimin ne PHP</h3>";?>
+        </div>
+    </body>
+</html>
